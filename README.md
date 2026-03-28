3df8c6c890afa5b1d6d94228144b180568446397
David Valencia (Camus) <cedadcamus@users.noreply.github.com>
2026-03-28 17:05:05 -0500

    README and build batch file

---
+++ README.md
@@ -0,0 +1,3 @@
+# TIC TAC TOE
+
+A simple tic tac toe game made using [Camus Engine](https://github.com/cedadcamus/camus)
--- build.bat
+++ build.bat
@@ -1,18 +1,12 @@
 copy lib\SDL3-win32-x64\SDL3.dll SDL3.dll
 copy lib\SDL3-win32-x64\SDL3.dll build\win_x64\SDL3.dll
 
-copy lib\SDL3_ttf-win32-x64\SDL3_ttf.dll SDL3_ttf.dll
-copy lib\SDL3_ttf-win32-x64\SDL3_ttf.dll build\win_x64\SDL3_ttf.dll
+copy "Doto-VariableFont_ROND,wght.ttf" build\win_x64\"Doto-VariableFont_ROND,wght.ttf"
+copy Jersey10-Regular.ttf build\win_x64\Jersey10-Regular.ttf
 
-odin build . -o:speed -target:windows_amd64 -out:%cd%\build\win_x64\tictactoe.exe
+odin build . -o:speed -out:%cd%\build\win_x64\tictactoe.exe
 
-copy lib\SDL3-win32-x86\SDL3.dll SDL3.dll
-copy lib\SDL3-win32-x86\SDL3.dll build\win_x86\SDL3.dll
-
-copy lib\SDL3_ttf-win32-x86\SDL3_ttf.dll SDL3_ttf.dll
-copy lib\SDL3_ttf-win32-x86\SDL3_ttf.dll build\win_x86\SDL3_ttf.dll
-
-D:\odin_x86\odin.exe build . -o:speed -target:windows_i386 -out:%cd%\build\win_x86\tictactoe.exe
+butler push build\win_x64 cedad-camus/tic-tac-toe:windows-x64
 
 del SDL3.dll
 del SDL3_ttf.dll
